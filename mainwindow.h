b3b3084382b549f9d53ed529fc14bda3d9774d73
David Brown <david@davidbrownucf.com>
2012-02-23 23:20:41 -0500

    end of day dump

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "scannerthread.h"
 
 namespace Ui {
     class MainWindow;
@@ -14,6 +15,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QByteArray getHexFootprint();
+    scannerThread *ScannerThread;
+
+private slots:
+    void on_btnOpenFile_clicked();
+    void onOutputResults();
+    void on_btnStopScan_clicked();
+
+    void on_btnScan_clicked();
+
+    void on_btnClearResults_clicked();
 
 private:
     Ui::MainWindow *ui;
